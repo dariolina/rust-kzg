dad3264716a53dd651805ebcb8f52c528a40dba0
Dariia Porechna <35382599+dariolina@users.noreply.github.com>
2023-02-17 11:08:17 +0100

    Test for extension and recover from plots

--- blst-from-scratch/tests/local_tests/local_recovery.rs
+++ blst-from-scratch/tests/local_tests/local_recovery.rs
@@ -1,4 +1,4 @@
-use kzg::{FFTFr, FFTSettings, Fr, Poly, PolyRecover};
+use kzg::{FFTFr, FFTSettings, Fr, Poly, PolyRecover, DAS};
 use rand::rngs::StdRng;
 use rand::{RngCore, SeedableRng};
 use std::convert::TryInto;
@@ -157,4 +157,87 @@ pub fn recover_random<
             }
         }
     }
+}
+
+pub fn recover_proven<
+    TFr: Fr,
+    TFFTSettings: FFTSettings<TFr> + FFTFr<TFr> + DAS<TFr>,
+    TPoly: Poly<TFr>,
+    TPolyRecover: PolyRecover<TFr, TPoly, TFFTSettings>,
+>() {
+    //log of desired size, double the record size
+    let ext_scale: usize = 16;
+
+    //initialize fft for extended record
+    let fft_ext = TFFTSettings::new(ext_scale);
+    assert!(fft_ext.is_ok());
+    let fft_ext_settings: TFFTSettings = fft_ext.unwrap();
+    let max_width: usize = fft_ext_settings.get_max_width();
+
+    //initialize fft for source record
+    let fft_src = TFFTSettings::new(ext_scale-1);
+    assert!(fft_src.is_ok());
+    let fft_src_settings: TFFTSettings = fft_src.unwrap();
+
+    //set random record chunks for the record size
+    let mut source = Vec::new();
+    for _i in 0..(max_width / 2) {
+            source.push(TFr::rand());
+        }
+
+    //Plotting:
+    //compute as many parity chunks from source chunks
+    let parity = fft_ext_settings.das_fft_extension(&source).unwrap();
+
+    //combine into extended record, even chunks are source, odd are parity    
+    let mut extended = Vec::new();
+            for i in (0..max_width).step_by(2) {
+                extended.push(source[i / 2].clone());
+                extended.push(parity[i / 2].clone());
+            }
+    //assert that polynomial interpolated from source and extended data match
+    let poly_from_ext = fft_ext_settings.fft_fr(&extended, true).unwrap();
+    let poly_from_src = fft_src_settings.fft_fr(&source,true).unwrap();  
+    for i in 0..max_width/2 {
+        assert!(poly_from_ext[i].equals(&poly_from_src[i]));  
+    };
+    //second half of coeffs of extednded poly is all 0
+    for i in max_width/2..max_width {
+        assert!(poly_from_ext[i].is_zero());  
+    };
+    //(imagine) query Chia for 2^15 proofs_of_space
+    //keep only proven indices (random for test)
+    let known_ratio = 0.5;
+    let known: usize = ((max_width as f32) * known_ratio) as usize;
+    let mut proven = vec![Some(TFr::default()); max_width];
+    random_missing(&mut proven, &extended, max_width, known);
+
+    //proven gets xored with Chia PoS and stored
+
+    //Extraction:
+    //recover the source data from proven chunks
+    //the function recover_poly_from_samples returns evaluations, but we would need to modify it to return coefficients for efficient farming
+    let recovered_data = TPolyRecover::recover_poly_from_samples(&proven, &fft_ext_settings).unwrap();
+    //assert that recovered data is equal to extended data
+    for i in 0..max_width {
+        assert!(recovered_data.get_coeff_at(i).equals(&extended[i]));
+    }
+    //source data is at even indices of recovered data
+    for i in 0..max_width/2 {
+        assert!(recovered_data.get_coeff_at(2*i).equals(&source[i]));
+    }
+
+    //Farming
+    //evaluate the recovered polynomial at source points
+    //the function recover_poly_from_samples above returns evaluations
+    //but we would need to modify it to return coefficients for efficient farming
+    //to avoid doing this extra FFT when farming (time-critical) 
+    //and push it to Extraction instead (not time-critical)
+
+    let recovered_poly = fft_ext_settings.fft_fr(recovered_data.get_coeffs(), true).unwrap();
+    //assert recovered poly is same as source poly
+    for i in 0..max_width/2 {
+         assert!(poly_from_src[i].equals(&recovered_poly[i]));
+    }
+   
 }
--- blst-from-scratch/tests/recovery.rs
+++ blst-from-scratch/tests/recovery.rs
@@ -1,11 +1,11 @@
-// #[path = "./local_tests/local_recovery.rs"]
-// pub mod local_recovery;
+ #[path = "./local_tests/local_recovery.rs"]
+ pub mod local_recovery;
 
 #[cfg(test)]
 pub mod tests {
-    use kzg_bench::tests::recover::{recover_random, recover_simple};
+    //use kzg_bench::tests::recover::{recover_random, recover_simple};
     // uncomment to use the local tests
-    //use crate::local_recovery::{recover_random, recover_simple};
+    use crate::local_recovery::{recover_random, recover_simple, recover_proven};
 
     use blst_from_scratch::types::fft_settings::FsFFTSettings;
     use blst_from_scratch::types::fr::FsFr;
@@ -20,5 +20,10 @@ pub mod tests {
     #[test]
     fn recover_random_() {
         recover_random::<FsFr, FsFFTSettings, FsPoly, FsPoly>()
+    }
+
+    #[test]
+    fn recover_proven_() {
+        recover_proven::<FsFr, FsFFTSettings, FsPoly, FsPoly>()
     }
 }
